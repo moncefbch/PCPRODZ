783d15e09a787973fc3200e409bf37992572e2ef
keziz mouayed <mouayed0001@gmail.com>
2022-04-03 14:49:34 +0100

    Commit_7

--- trash/main.py
+++ trash/main.py
@@ -1,6 +1,6 @@
 
 
-with open('wilaya', 'r+') as file:
+with open('wilaya.txt', 'r+') as file:
     with open("out.txt", 'w') as file2:
         content = file.readlines()
         for wilaya in content:
