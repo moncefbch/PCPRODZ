83939d855ca483a34fa9e4ee5bbedae5bc0aa325
keziz mouayed <mouayed0001@gmail.com>
2022-04-03 13:50:03 +0100

    Commit_5

---
+++ trash/main.py
@@ -0,0 +1,8 @@
+
+
+with open('wilaya', 'r+') as file:
+    with open("out.txt", 'w') as file2:
+        content = file.readlines()
+        for wilaya in content:
+            wilaya = "<option>" + wilaya[:-1] + "</option>"
+            file2.write(wilaya)
